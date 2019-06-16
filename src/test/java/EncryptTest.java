c31f56c3fc8da44d8b176b3fff27df805905f3c2
Annette Odingo <odingoannette@gmail.com>
2019-06-16 10:42:51 +0300

    Test the first behavior; the constructor

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptTest {
-
+    @Test
+    public void newEncrypt_instantiatesCorrectly() {
+        Encrypt testEncrypt = new Encrypt(1, "Apiyo");
+        assertTrue(testEncrypt instanceof Encrypt);
+    }
 }

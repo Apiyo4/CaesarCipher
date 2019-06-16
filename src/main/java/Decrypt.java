975d727ef62e5ac057eab2cc4fb426365560e0e2
Annette Odingo <odingoannette@gmail.com>
2019-06-16 14:00:23 +0300

    Add a test to see if it can save name

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -2,6 +2,12 @@ public class Decrypt {
     private int mKey;
     private String mName;
 
-    public Decrypt(){
+    public Decrypt(int mKey, String mName) {
+        this.mKey = mKey;
+        this.mName = mName;
+    }
+
+    public String getmName() {
+        return mName;
     }
 }
--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.*;
 
 public class DecryptTest {
     @Test
-    public void newEncrypt_instantiatesCorrectly() {
+    public void newDecrypt_instantiatesCorrectly() {
         Encrypt testEncrypt = new Encrypt(1, "Apiyo");
         assertTrue(testEncrypt instanceof Encrypt);
+    }
+    @Test
+    public void newDecrypt_getsName_Apiyo() {
+        Encrypt testEncrypt = new Encrypt(1, "Apiyo");
+        assertEquals("Apiyo", testEncrypt.getName());
+
     }
 }

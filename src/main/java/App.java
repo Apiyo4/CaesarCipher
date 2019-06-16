730c312e2fbfb2461abfb17f01ae4733715007e9
Annette Odingo <odingoannette@gmail.com>
2019-06-16 14:37:54 +0300

    Give the user options and a chance to make a choice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,13 @@
 import java.io.Console;
+import java.sql.SQLOutput;
 
 public class App {
     public static void main(String[] args) {
         Console myConsole = System.console();
+        System.out.println("Welcome to Caesar Cipher!");
+        System.out.println("Feel free to choose any of the following options:");
+        System.out.println("1. Encrypt a word || 2. Decrypt a word || 3. Quit");
+        String choice = myConsole.readLine();
 
     }
 }

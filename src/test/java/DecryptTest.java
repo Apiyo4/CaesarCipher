2e5812346b3fd010527341881ec47511a5bffa87
Annette Odingo <odingoannette@gmail.com>
2019-06-16 13:57:22 +0300

    Add a test for constructor

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecryptTest {
-
+    @Test
+    public void newEncrypt_instantiatesCorrectly() {
+        Encrypt testEncrypt = new Encrypt(1, "Apiyo");
+        assertTrue(testEncrypt instanceof Encrypt);
+    }
 }

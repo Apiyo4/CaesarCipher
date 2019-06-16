07a46d33c081523979f229bc384260d1ab1fe52d
Annette Odingo <odingoannette@gmail.com>
2019-06-16 10:34:09 +0300

    Create a class Encrypt and add a constructor

---
+++ src/main/java/Encrypt.java
@@ -0,0 +1,9 @@
+public class Encrypt {
+    private int mKey;
+    private String mName;
+
+    public Encrypt(int mKey, String mName) {
+        this.mKey = mKey;
+        this.mName = mName;
+    }
+}
